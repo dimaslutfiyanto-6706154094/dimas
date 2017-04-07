934075d032463a82f2f477af97c69f88cbb1f77c
Dimas Lutfiyanto <lutfiyantom@gmail.com>
2017-04-07 10:18:00 +0700

    Update Jurnal1.java

--- Jurnal1/src/jurnal1/Jurnal1.java
+++ Jurnal1/src/jurnal1/Jurnal1.java
@@ -16,26 +16,38 @@ public class Jurnal1 {
     /**
      * @param args the command line arguments
      */
+     private int[] a;
+
+    private int fib(int i) {
+        assert (i>=0);
+
+        if (a[i]==0) {
+            if (i==0 || i==1) {
+                a[i] = 1;
+            } else {
+                a[i] = fib(i - 2) + fib(i - 1);
+            }
+        }
+
+        return a[i];
+    }
+
+    public Jurnal1(int numberTerms) {
+        if (numberTerms<2) throw new IllegalArgumentException("expect at least 2 terms for a Fibonacci sequence");
+        a = new int[numberTerms];
+    }
+
+    public void print() {
+        for (int i=a.length; i!=0; i--) {
+            System.out.println(fib(i-1));
+        }
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Scanner show = new Scanner(System.in);
-        System.out.print("Masukan Jumlah Deret Fibonacci ");
-        int n = show.nextInt();
-        long fib[] = new long[n];
-        
-        int i = 1;
-        fib[0] = 0;
-        fib[1] = 1;
-         
-        for( i = 2; i < n; i++) {
-            fib[i] = fib[i-1] + fib[i-2];
-        }
-         
-        for ( i = 0; i < n; i++) {
-            System.out.print(fib[i] +  " ");
-        }
-        
+        Jurnal1 f = new Jurnal1(10);
+        f.print();        
     }
     
 }
